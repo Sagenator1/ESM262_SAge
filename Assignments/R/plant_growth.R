aa49fc4a522508f3b6c0b139bc778faea319e243
Sage <sage@ucsb.edu>
2023-03-08 19:10:36 -0800

    Work in progress

--- Assignments/R/plant_growth.R
+++ Assignments/R/plant_growth.R
@@ -4,7 +4,7 @@
 #' @param water
 #' 
 #' 
-plant_growth = function(sun, water){
+plant_growth = function(sun, water, pot = 1){
   growth = 2*sun + 0.5*water
   leaves = 0.1*growth
   return(list(growth, leaves))
