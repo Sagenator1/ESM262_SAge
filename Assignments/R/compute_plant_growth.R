322a5436317fce224eb0419a98c780b5fdcb7dcd
daijaodom <daijaodom@bren.ucsb.edu>
2023-03-11 15:01:10 -0800

    Updated test function and commented out error checking

--- Assignments/R/compute_plant_growth.R
+++ Assignments/R/compute_plant_growth.R
@@ -3,22 +3,22 @@
 #' Compute the growth of a plant over time
 #' @param sun number of sun hours per week
 #' @param water number of watering events per week
-#' @param pot size of pot the plant is growing in
+#' @param pot size of pot the plant is growing in (diameter in feet)
 #' @return height of plant (in mm) and number of leaves 
 #' 
 
 compute_plant_growth = function(sun, water, pot = 1){
   
   #start with some error checking
-  if(sun <0 ){
-    stop("Number of sun hours cannot be less than 0")
-  }
+  #if(sun <0 ){
+    #stop("Number of sun hours cannot be less than 0")
+  #}
   
-  if(water <0 ){
-    stop("Number of watering events cannot be less than 0")
-  }
+  #if(water <0 ){
+    #stop("Number of watering events cannot be less than 0")
+  #}
   
-  growth = 2*sun + 0.5*water
+  growth = 2*sun + 0.5*water + pot
   leaves = 0.1*growth
   
   return(list(growth, leaves))
--- Assignments/tests/test_compute_plant_growth.R
+++ Assignments/tests/test_compute_plant_growth.R
@@ -1,4 +1,9 @@
-test_that("compute_plant_growth_works", {
-  expect_equal()
-}
-          )
+test_that(
+  "compute_plant_growth_works", 
+  {
+    # expect_true((summary(water)["Max."]-summary(water)["Min."]) > 0)
+    # if the plant never gets water for 7 days then both should be zero
+    water_data = rep(0, times=7)
+    expect_that(compute_plant_growth(water_data)$totalwater, equals(0))
+    }
+  )
