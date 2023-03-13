e3093520f806972a524080ff898bd83031d92960
kristinart <kristinart21@gmail.com>
2023-03-12 19:14:01 -0700

    Made a few formatting tweaks and added a caption for the plot.

--- Assignments/R/compute_plant_growth.R
+++ Assignments/R/compute_plant_growth.R
@@ -4,7 +4,8 @@
 #' @param sun number of sun hours per week
 #' @param water number of watering events per week
 #' @param pot size of pot (diameter in feet) the plant is growing in 
-#' @return height of plant (in mm) and number of leaves 
+#' @return height of plant (in mm) 
+#' @return number of leaves (n)
 #' 
 
 compute_plant_growth = function(sun, water, pot = 1){
@@ -13,6 +14,7 @@ compute_plant_growth = function(sun, water, pot = 1){
   sun = ifelse((sun < 0 ), return("Sun cannot be less than 0"), sun)
   water = ifelse((water < 0), return("Water cannot be less than 0"), water)
   
+  #compute plant growth and new leaves
   growth = (2*sun + 0.5*water)*pot
   leaves = 0.1*growth
   
